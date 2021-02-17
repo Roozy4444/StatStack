90c97ce7e36e4ef6d8390facba98c3476e6a3b62
Will Howell <will_howell@Wills-MacBook-Pro.local>
2021-02-17 12:54:47 -0500

    CSS styling to home page and sign in forms

---
+++ client/src/components/ReqSignIn.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ReqSignIn = props => {
+  return (
+    <div className= 'required-sign-in'>
+      <h1>Please Sign In to View Your Profile</h1>
+      <img src='https://media.giphy.com/media/26oqUoUe23WPHnlvOP/giphy.gif' alt='Fred Flintsone swinging a baseball bat' className='flintstone' />
+    </div>
+  )
+}
+
+export default ReqSignIn
