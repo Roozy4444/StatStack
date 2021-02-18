ba7bbeb55176620463eae0316ae45f50e01e9717
Will Howell <will_howell@Wills-MacBook-Pro.local>
2021-02-18 12:46:49 -0500

    worked on show pages

---
+++ client/src/components/PastTenShow.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react"
+import BattingTile from './BattingTile'
+
+const PastTenShow = props => {
+  return (
+    <div>
+      <h1>Combined 10 Game Stats</h1>
+    </div>
+  )
+}
+
+export default PastTenShow
